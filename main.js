6175897426008bcbf18ebdbd6c8c770cf5ed891b
Trungdat35 <dotrungdat.mda@gmail.com>
2023-08-14 22:41:57 +0700

    dat

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-alert("Hi dat");
-var  age = 23;
-function thongBao(){
-    alert(`Nam nay ${age} tuoi`)
-}
+const thongBao = async () => {
+  var imgElement = await document.getElementById("imgElement");
+  imgElement.setAttribute(
+    "src",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoTtb4yyUuall0h3P6abzoHgErmtJRYO1HwK60mbuMag&s"
+  );
+  var imgElement2 = await document.getElementById("imgElement2");
+  imgElement2.setAttribute(
+    "src",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoTtb4yyUuall0h3P6abzoHgErmtJRYO1HwK60mbuMag&s"
+  );
+  imgElement.setAttribute("width", "200");
+  imgElement.setAttribute("height", "150");
+};
